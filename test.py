ea2ebfe8421eefea1f08e46f2a0ee9019cbb17e9
yunw <1119142470@qq.com>
2020-04-27 07:01:29 -0700

    modify

--- test.py
+++ test.py
@@ -5,21 +5,8 @@ domainlist = []
 class Domain:
 	def __init__(self,_name,_label, _min, _max, _numip, _ipset):
 		self.name = _name
-		self.label = _label
-		self.ttlmin = _min
-		self.ttlmax = _max
-		self.numip = _numip
-		self.ipset = _ipset
+		self.label = _label	
 
-
-	def returnData(self):
-		return [self.ttlmin, self.ttlmax, self.numip]
-
-	def returnLabel(self):
-		if self.label == "good":
-			return 0
-		else:
-			return 1
 		
 def processData(name):
 	numofn=0;
